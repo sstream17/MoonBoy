93670e59425969e2e57e7cc741b505e2101f39bd
Spencer Stream <sprewstream@gmail.com>
2019-05-05 23:48:20 -0500

    Added a dumb follow AI to the enemies

--- Moon Boy/Assets/Scripts/Enemy.cs
+++ Moon Boy/Assets/Scripts/Enemy.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
 	public int health = 100;
+	public float speed;
+	public float startingDistance;
+	public float stoppingDistance;
+	public float retreatDistance;
+
+	public Transform player;
 
 	public GameObject deathEffect;
+
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+	void Update() {
+		float distance = Vector2.Distance(transform.position, player.position);
+		if (distance < startingDistance && distance > stoppingDistance) {
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+		}
+		else if (distance < stoppingDistance && distance > retreatDistance) {
+			transform.position = this.transform.position;
+		}
+		else if (distance < retreatDistance) {
+			transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+		}
+	}
 
 	public void TakeDamage (int damage) {
 		health -= damage;
