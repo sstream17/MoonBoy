310c829262f7d89ae880173fc16060e7c15b4327
Spencer Stream <sprewstream@gmail.com>
2019-05-09 01:05:50 -0500

    Refactored code to include enemy scripts in one folder. Added ability for enemy to shoot at player

---
+++ Moon Boy/Assets/Scripts/Enemy/EnemyBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    Weapon weapon;
+	public Rigidbody2D rb;
+	public GameObject impactEffect;
+
+	// Use this for initialization
+	void Start () {
+        weapon = GetComponentInParent<EnemyWeapon>().weapon;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float deltaY = player.transform.position.y - rb.position.y;
+        float deltaX = player.transform.position.x - rb.position.x;
+        float theta = Mathf.Atan(deltaY / deltaX);
+        Vector2 targetVelocity = new Vector2(-weapon.speed * Mathf.Cos(theta), -weapon.speed * Mathf.Sin(theta));
+        rb.velocity = targetVelocity;
+	}
+
+	void OnTriggerEnter2D (Collider2D hitInfo) {
+		Player player = hitInfo.GetComponent<Player>();
+		if (player != null) {
+			player.TakeDamage(weapon.damage);
+		}
+
+		GameObject impactEffectClone = Instantiate(impactEffect, transform.position, transform.rotation);
+
+		Destroy(gameObject);
+		Destroy(impactEffectClone, 5);
+	}
+}
---
+++ Moon Boy/Assets/Scripts/Enemy/EnemyWeapon.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour {
+	public Weapon weapon;
+	public Transform firePoint;
+    public Transform target;
+    [HideInInspector]
+    public float startingDistance;
+
+	private bool hasShot = false;
+	private bool allowShoot = true;
+    private bool searchingForPlayer = false;
+    private LayerMask layerMask;
+
+    private Timer timer;
+
+
+
+	private void HandleTimer(System.Object source, ElapsedEventArgs e) {
+		allowShoot = true;
+    }
+
+
+	public void SetTimer() {
+		timer = new Timer(weapon.fireRate * 1000f);
+		timer.Elapsed += HandleTimer;
+		timer.AutoReset = false;
+	}
+
+
+	void Shoot() {
+		GameObject bulletClone = Instantiate(weapon.enemyBulletPrefab, firePoint.position, firePoint.rotation, transform);
+		Destroy(bulletClone, 10);
+	}
+
+
+    IEnumerator SearchForPlayer() {
+        GameObject searchResult = GameObject.FindGameObjectWithTag("Player");
+        if (searchResult == null) {
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(SearchForPlayer());
+        }
+        else {
+            searchingForPlayer = false;
+            target = searchResult.transform;
+            yield return false;
+        }
+    }
+
+
+    void AttemptShot() {
+        RaycastHit2D hitInfo = Physics2D.Linecast(firePoint.position, target.position, layerMask);
+        if (hitInfo && (firePoint.position - target.position).magnitude <= startingDistance) {
+            Player player = hitInfo.transform.GetComponent<Player>();
+            if (player != null) {
+                allowShoot = false;
+                Shoot();
+                timer.Start();
+            }
+        }
+    }
+
+
+    void Start() {
+        startingDistance = GetComponentInParent<EnemyAI>().startingDistance;
+        SetTimer();
+        layerMask = LayerMask.GetMask("Player", "Ground", "Enemy");
+        if (target == null) {
+            if (!searchingForPlayer) {
+                searchingForPlayer = true;
+                StartCoroutine(SearchForPlayer());
+            }
+            return;
+        }
+	}
+
+
+	void Update() {
+        bool playerInFront = target.position.x < firePoint.position.x;
+		if (allowShoot && playerInFront) {
+			AttemptShot();
+		}
+	}
+}
