08a3b8fdfd6e09cfd624dc4c7536a6800781fe87
Spencer Stream <sprewstream@gmail.com>
2019-05-09 01:29:32 -0500

    Player health is now tracked in the UI. Enemies now do less damage to the player

--- Moon Boy/Assets/Scripts/Player.cs
+++ Moon Boy/Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+
+    public TextMeshProUGUI healthDisplay;
     public int health = 100;
+    public float damageModifier = 0.25f;
 
 	public GameObject deathEffect;
 
 
 	public void TakeDamage (int damage) {
-		health -= damage;
+		health -= (int) Mathf.Ceil(damage * damageModifier);
+        healthDisplay.text = health.ToString("0");
 
 		if (health <= 0) {
 			Die();
