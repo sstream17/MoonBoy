4fce4580ebb701fa3970ecbde817fd5fdf280b80
Spencer Stream <sprewstream@gmail.com>
2019-05-10 22:41:07 -0500

    Added a display name for each weapon scriptable object

--- Moon Boy/Assets/Scripts/Weapons/Weapon.cs
+++ Moon Boy/Assets/Scripts/Weapons/Weapon.cs
@@ -7,6 +7,7 @@ using TMPro;
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
 public class Weapon : ScriptableObject {
 
+	public string displayName;
     public int damage;
 	public float speed;
     public float fireRate;
