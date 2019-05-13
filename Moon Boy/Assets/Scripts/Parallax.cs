cad6e0c4bf718daee03ba1c62ec9c925fafa7ad4
Spencer Stream <sprewstream@gmail.com>
2019-05-13 16:39:34 -0500

    Added scripts for a parallax background and added distant ground to level 2 to test the effect

---
+++ Moon Boy/Assets/Scripts/Parallax.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public  Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1f;
+    public float smoothingY = 2f;
+
+    private Transform gameCamera;
+    private Vector3 previousCameraPosition;
+
+
+    void Awake() {
+        gameCamera = Camera.main.transform;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        previousCameraPosition = gameCamera.position;
+
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++) {
+            parallaxScales[i] = backgrounds[i].position.z;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        for (int i = 0; i < backgrounds.Length; i++) {
+            float parallaxX = (previousCameraPosition.x - gameCamera.position.x) * parallaxScales[i];
+            float parallaxY = (previousCameraPosition.y - gameCamera.position.y) * parallaxScales[i] * smoothingY;
+            float targetPositionX = backgrounds[i].position.x + parallaxX;
+            float targetPositionY = backgrounds[i].position.y + parallaxY;
+            Vector3 backgroundTargetPositon = new Vector3(targetPositionX, targetPositionY, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPositon, smoothing * Time.fixedDeltaTime);
+        }
+        previousCameraPosition = gameCamera.position;
+    }
+}
---
+++ Moon Boy/Assets/Scripts/ParallaxTiling.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D;
+
+public class ParallaxTiling : MonoBehaviour
+{
+
+    enum Direction {Right = 1, Left = -1};
+
+    public int offsetX = 0;
+
+    public bool hasLeftConnection = false;
+    public bool hasRightConnection = false;
+
+    private float spriteWidth = 60f;
+    private Camera gameCamera;
+    private Transform thisTransform;
+
+
+    void Awake() {
+        gameCamera = Camera.main;
+        thisTransform = transform;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        SpriteShapeRenderer spriteRenderer = GetComponent<SpriteShapeRenderer>();
+    }
+
+
+    void MakeConnection(Direction direction) {
+        Vector3 newPosition = new Vector3(thisTransform.position.x + (spriteWidth * (int) direction), thisTransform.position.y, thisTransform.position.z);
+        Transform newConnection = Instantiate(thisTransform, newPosition, thisTransform.rotation);
+        newConnection.parent = thisTransform;
+        if (direction == Direction.Right) {
+            newConnection.GetComponent<ParallaxTiling>().hasLeftConnection = true;
+        }
+        else {
+            newConnection.GetComponent<ParallaxTiling>().hasRightConnection = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (!hasLeftConnection || !hasRightConnection) {
+            float cameraHorizontalExtend = gameCamera.orthographicSize * Screen.width / Screen.height;
+            float edgeVisiblePositionRight = (thisTransform.position.x + spriteWidth / 2) - cameraHorizontalExtend;
+            float edgeVisiblePositionLeft = (thisTransform.position.x - spriteWidth / 2) + cameraHorizontalExtend;
+
+            if ((gameCamera.transform.position.x >= edgeVisiblePositionRight - offsetX) && !hasRightConnection) {
+                MakeConnection(Direction.Right);
+                hasRightConnection = true;
+            }
+            else if ((gameCamera.transform.position.x <= edgeVisiblePositionLeft + offsetX) && !hasLeftConnection) {
+                MakeConnection(Direction.Left);
+                hasLeftConnection = true;
+            }
+        }
+    }
+}
