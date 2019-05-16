84d2a139cac683c87da7cc0e25113b068d779492
Spencer Stream <sprewstream@gmail.com>
2019-05-16 17:25:04 -0500

    Created a pause menu and added a script to control it

---
+++ Moon Boy/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject controlsUI;
+    public GameObject pauseUI;
+
+
+    public void Pause() {
+        controlsUI.SetActive(false);
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+
+    public void Resume() {
+        pauseUI.SetActive(false);
+        controlsUI.SetActive(true);
+        Time.timeScale = 1f;
+    }
+}
