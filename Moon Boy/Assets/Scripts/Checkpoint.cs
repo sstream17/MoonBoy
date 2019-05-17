4746d4b2bd1aa74f6b54e63112779798880fe501
Spencer Stream <sprewstream@gmail.com>
2019-05-17 01:01:52 -0500

    Created a checkpoint which can move the respawn point as the player passes through it

---
+++ Moon Boy/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D hitInfo) {
+		Player player = hitInfo.GetComponent<Player>();
+		if (player != null) {
+            Vector2 newPosition = player.transform.position;
+            newPosition.y = newPosition.y + 1f;
+			GameControl.control.spawnPoint.position = newPosition;
+		}
+	}
+}
