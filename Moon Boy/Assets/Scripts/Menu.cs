23bc8fbfca9630af659de627d19c39e979b9ab6e
Spencer Stream <sprewstream@gmail.com>
2019-08-04 13:13:29 -0500

    Fixed bug causing lives and ammo to retain between playthroughs

--- Moon Boy/Assets/Scripts/Menu.cs
+++ Moon Boy/Assets/Scripts/Menu.cs
@@ -19,6 +19,8 @@ public class Menu : MonoBehaviour
 
 
     void Awake() {
+        GameControl.control.playerAmmo = 50;
+        GameControl.control.playerLives = 3;
         currentMenu = mainMenu;
         SwitchMenu(MenuScreen.Main);
     }
