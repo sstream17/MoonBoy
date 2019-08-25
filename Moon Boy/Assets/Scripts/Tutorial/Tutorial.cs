f0894c8f0178b6f93d5ccdc766eefec24a64ac6b
Spencer Stream <sprewstream@gmail.com>
2019-08-25 13:23:59 -0500

    Setup the animation suggesting the user to shoot the enemy. A second camera is activated so the enemy will always be in the same place once the user arrives

--- Moon Boy/Assets/Scripts/Tutorial/Tutorial.cs
+++ Moon Boy/Assets/Scripts/Tutorial/Tutorial.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
@@ -16,12 +17,17 @@ public class Tutorial : MonoBehaviour
 
     public GameObject Cursor;
     public Animator CursorAnimator;
-    public Vector2 CursorPosition_SwipeJoystick;
+    public Vector2 CursorPosition_Joystick;
     public Vector2 CursorPosition_Shoot;
     public Vector2 CursorPosition_Swap;
     public Vector2 CursorPosition_Toggle;
 
     public Weapon[] enemyWeapons;
+
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+
+    public enum Area { Shoot = 0, Swap = 1, Toggle = 2 };
 
     void Awake()
     {
@@ -43,9 +49,37 @@ public class Tutorial : MonoBehaviour
     private void StartJoystickAnimation()
     {
         JoystickTrigger.SetActive(true);
-        Cursor.transform.localPosition = CursorPosition_SwipeJoystick;
+        Cursor.transform.localPosition = CursorPosition_Joystick;
         Cursor.SetActive(true);
         CursorAnimator.SetTrigger("Joystick");
+    }
+
+    public void StartTapAnimation(Area area)
+    {
+        switch (area)
+        {
+            case Area.Shoot:
+                ShootTrigger.SetActive(true);
+                ShootArea.SetActive(true);
+                Cursor.transform.localPosition = CursorPosition_Shoot;
+                ActivateCameraTwo();
+                break;
+
+            case Area.Swap:
+                SwapTrigger.SetActive(true);
+                SwapButton.SetActive(true);
+                Cursor.transform.localPosition = CursorPosition_Swap;
+                break;
+
+            case Area.Toggle:
+                ToggleTrigger.SetActive(true);
+                ToggleButton.SetActive(true);
+                Cursor.transform.localPosition = CursorPosition_Toggle;
+                break;
+        }
+
+        Cursor.SetActive(true);
+        CursorAnimator.SetTrigger("Tap");
     }
 
     public void StopAnimation()
@@ -61,5 +95,17 @@ public class Tutorial : MonoBehaviour
             PlayerPrefs.SetInt(PlayerPrefConstants.Tutorial, -1);
             LevelTransition.FadeToLevel(0);
         }
+    }
+
+    public void ActivateCameraTwo()
+    {
+        cameraTwo.SetActive(true);
+        cameraOne.SetActive(false);
+    }
+
+    public void ActivateCameraOne()
+    {
+        cameraOne.SetActive(true);
+        cameraTwo.SetActive(false);
     }
 }
