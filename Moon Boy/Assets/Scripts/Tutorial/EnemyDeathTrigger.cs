f1a89845642e703844ce91916cee3379a1a390e1
Spencer Stream <sprewstream@gmail.com>
2019-08-25 17:44:29 -0500

    Fixed bug that was preventing the user from swapping weapons. The trigger to swap is shrunk when it spawns and then expanded when it is in range

---
+++ Moon Boy/Assets/Scripts/Tutorial/EnemyDeathTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EnemyDeathTrigger : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        ShrinkWeaponSpawn();
+    }
+
+    private void ShrinkWeaponSpawn()
+    {
+        GameObject weaponSpawn = GameObject.FindGameObjectWithTag("Collectible");
+        if (weaponSpawn != null)
+        {
+            GameObject trigger = weaponSpawn.transform.GetChild(0).gameObject;
+            if (trigger != null)
+            {
+                CircleCollider2D circleCollider = trigger.GetComponent<CircleCollider2D>();
+                if (circleCollider != null)
+                {
+                    circleCollider.radius = 0f;
+                }
+            }
+        }
+    }
+}
--- Moon Boy/Assets/Scripts/Tutorial/TutorialTrigger.cs
+++ Moon Boy/Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -25,11 +25,29 @@ public class TutorialTrigger : MonoBehaviour
                     break;
 
                 case Tutorial.Area.Swap:
+                    ExpandWeaponSpawn();
                     Tutorial.ActivateCamera(2);
                     break;
             }
 
             StartCoroutine(Tutorial.WaitToStartAnimation(time, Area, Tutorial.StartTapAnimation));
+        }
+    }
+
+    private void ExpandWeaponSpawn()
+    {
+        GameObject weaponSpawn = GameObject.FindGameObjectWithTag("Collectible");
+        if (weaponSpawn != null)
+        {
+            GameObject trigger = weaponSpawn.transform.GetChild(0).gameObject;
+            if (trigger != null)
+            {
+                CircleCollider2D circleCollider = trigger.GetComponent<CircleCollider2D>();
+                if (circleCollider != null)
+                {
+                    circleCollider.radius = 0.8f;
+                }
+            }
         }
     }
 }
