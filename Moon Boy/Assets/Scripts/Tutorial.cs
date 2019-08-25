c0f38d301a96e6c5b3023c90147c7da157dc336a
Spencer Stream <sprewstream@gmail.com>
2019-08-25 00:59:28 -0500

    Setup the tutorial to display the joystick animation after waiting 2 seconds

--- Moon Boy/Assets/Scripts/Tutorial.cs
+++ Moon Boy/Assets/Scripts/Tutorial.cs
@@ -1,8 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
     public LevelTransition LevelTransition;
+
+    public GameObject Cursor;
+
+    public Animator CursorAnimator;
+
+    public Vector2 CursorPosition_SwipeJoystick;
+
+
+    IEnumerator WaitToStartAnimation()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        StartJoystickAnimation();
+    }
+
+    void Start()
+    {
+        StartCoroutine(WaitToStartAnimation());
+    }
+
+    private void StartJoystickAnimation()
+    {
+        Cursor.transform.localPosition = CursorPosition_SwipeJoystick;
+        Cursor.SetActive(true);
+        CursorAnimator.SetTrigger("Joystick");
+    }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
