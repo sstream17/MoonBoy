f74882e677074792a4de755980299ada6447db45
Spencer Stream <sprewstream@gmail.com>
2019-08-25 12:48:18 -0500

    Created an object to trigger specific tutorial events

---
+++ Moon Boy/Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public Tutorial Tutorial;
+    public Tutorial.Area Area;
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Player player = hitInfo.GetComponent<Player>();
+        if (player != null)
+        {
+            PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            playerMovement.animator.SetFloat("Speed", 0f);
+            playerMovement.animator.SetFloat("VerticalSpeed", 0f);
+            playerMovement.enabled = false;
+            Tutorial.StartTapAnimation(Area);
+        }
+    }
+}
