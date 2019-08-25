da760a39668d4b4bb949f7f7aeeef275d54ab337
Spencer Stream <sprewstream@gmail.com>
2019-08-25 11:08:12 -0500

    Put Tutorial scripts in their own folder

--- Moon Boy/Assets/Scripts/Tutorial/Tutorial.cs
+++ Moon Boy/Assets/Scripts/Tutorial/Tutorial.cs
@@ -5,11 +5,28 @@ public class Tutorial : MonoBehaviour
 {
     public LevelTransition LevelTransition;
 
+    public GameObject ShootArea;
+    public GameObject SwapButton;
+    public GameObject ToggleButton;
+
+    public GameObject JoystickTrigger;
+    public GameObject ShootTrigger;
+    public GameObject SwapTrigger;
+    public GameObject ToggleTrigger;
+
     public GameObject Cursor;
-
     public Animator CursorAnimator;
-
     public Vector2 CursorPosition_SwipeJoystick;
+    public Vector2 CursorPosition_Shoot;
+    public Vector2 CursorPosition_Swap;
+    public Vector2 CursorPosition_Toggle;
+
+    public Weapon[] enemyWeapons;
+
+    void Awake()
+    {
+        GameControl.control.enemyWeapons = enemyWeapons;
+    }
 
 
     IEnumerator WaitToStartAnimation()
@@ -25,9 +42,15 @@ public class Tutorial : MonoBehaviour
 
     private void StartJoystickAnimation()
     {
+        JoystickTrigger.SetActive(true);
         Cursor.transform.localPosition = CursorPosition_SwipeJoystick;
         Cursor.SetActive(true);
         CursorAnimator.SetTrigger("Joystick");
+    }
+
+    public void StopAnimation()
+    {
+        CursorAnimator.SetTrigger("Hide");
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
---
+++ Moon Boy/Assets/Scripts/Tutorial/TutorialObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class TutorialObject : MonoBehaviour
+{
+    public Tutorial Tutorial;
+
+    private RectTransform rectTransform;
+    private Vector3[] corners;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        corners = new Vector3[4];
+        rectTransform.GetWorldCorners(corners);
+    }
+
+    private void Update()
+    {
+        var touches = Input.touchCount;
+        for (int i = 0; i < touches; i++)
+        {
+            var touch = Input.GetTouch(i);
+            bool objectTouched = TouchIsInBounds(touch.position, corners[0], corners[2]);
+            if (objectTouched)
+            {
+                Tutorial.StopAnimation();
+                gameObject.SetActive(false);
+                break;
+            }
+        }
+    }
+
+    private bool TouchIsInBounds(Vector2 touch, Vector2 minBound, Vector2 maxBound)
+    {
+        if (touch.x < minBound.x || touch.x > maxBound.x)
+        {
+            return false;
+        }
+
+        if (touch.y < minBound.y || touch.y > maxBound.y)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
