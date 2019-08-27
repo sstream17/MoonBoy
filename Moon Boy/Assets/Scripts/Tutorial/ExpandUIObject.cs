26633de1c54f356398e8af66ffcc769a6d529a2c
Spencer Stream <sprewstream@gmail.com>
2019-08-27 18:06:11 -0500

    Added animations for expanding all of the UI user controls

---
+++ Moon Boy/Assets/Scripts/Tutorial/ExpandUIObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandUIObject : MonoBehaviour
+{
+    public enum TutorialObject { Joystick = 0, Swap = 1, Toggle = 2 };
+
+    public TutorialObject ThisTutorialObject;
+
+    public Animator Animator;
+
+    private void OnEnable()
+    {
+        switch (ThisTutorialObject)
+        {
+            case TutorialObject.Joystick:
+                Animator.Play("ExpandJoystick");
+                break;
+
+            case TutorialObject.Swap:
+                Animator.Play("ExpandSwapButton");
+                break;
+
+            case TutorialObject.Toggle:
+                Animator.Play("ExpandToggleButton");
+                break;
+        }
+    }
+}
