cb8e8992cf1d335db58a87f9adebf98213010d5e
Spencer Stream <sprewstream@gmail.com>
2019-12-31 09:44:27 -0600

    Adding a script to detect touches within the Shoot Area, rather than using the built-in button script

---
+++ Moon Boy/Assets/Scripts/Weapons/ShootArea.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public PrefabWeapon PrefabWeapon;
+    public bool buttonPressed;
+    public Vector2 pointerPosition;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+        pointerPosition = eventData.pressPosition;
+        PrefabWeapon.SendShoot();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
+}
