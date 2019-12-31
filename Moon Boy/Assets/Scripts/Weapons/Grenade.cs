f05255c70fb5851c28cc08232dee0889d23b8ba7
Spencer Stream <sprewstream@gmail.com>
2019-12-31 10:03:10 -0600

    Updating the Grenade script to use the touch position of the new ShootArea script to aim the grenades

--- Moon Boy/Assets/Scripts/Weapons/Grenade.cs
+++ Moon Boy/Assets/Scripts/Weapons/Grenade.cs
@@ -2,56 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grenade : MonoBehaviour {
+public class Grenade : MonoBehaviour
+{
 
-	public float speed = 15f;
-	public int damage = 100;
-	public float blastRadius = 5f;
-	public float delay = 5f;
-	public Rigidbody2D rb;
-	public GameObject impactEffect;
-	
-	float countdown;
-	bool hasExploded = false;
+    public float speed = 15f;
+    public int damage = 100;
+    public float blastRadius = 5f;
+    public float delay = 5f;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
 
-	// Use this for initialization
-	void Start () {
-		if (Input.touchCount > 0) {
-			foreach (Touch touch in Input.touches) {
-				if (touch.phase == TouchPhase.Ended) {
-					countdown = delay;
-					Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-					touchPosition.z = 0f;
-					float deltaY = touchPosition.y - rb.position.y;
-					float deltaX = touchPosition.x - rb.position.x;
-					float theta = Mathf.Atan(deltaY / deltaX);
-					Vector2 targetVelocity = new Vector2(speed * Mathf.Cos(theta), deltaY);
-					rb.velocity = targetVelocity;
-				}
-			}
-		}
-	}
+    float countdown;
+    bool hasExploded = false;
 
-	void Explode() {
-		GameObject impactEffectClone = Instantiate(impactEffect, transform.position, transform.rotation);
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, blastRadius);
-		foreach (Collider2D nearbyObject in colliders) {
-			Enemy enemy = nearbyObject.GetComponent<Enemy>();
-			if (enemy != null) {
-				enemy.TakeDamage(damage);
-			}
-		}
-		Destroy(gameObject);
-		Destroy(impactEffectClone, 5);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        ShootArea shootArea = GameObject.FindGameObjectWithTag("ShootArea").GetComponent<ShootArea>();
+        if (shootArea.buttonPressed)
+        {
+            countdown = delay;
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(shootArea.pointerPosition);
+            touchPosition.z = 0f;
+            float deltaY = touchPosition.y - rb.position.y;
+            float deltaX = touchPosition.x - rb.position.x;
+            float theta = Mathf.Atan(deltaY / deltaX);
+            Vector2 targetVelocity = new Vector2(speed * Mathf.Cos(theta), deltaY);
+            rb.velocity = targetVelocity;
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
-		countdown = countdown - Time.deltaTime;
-		if (countdown <= 0f && !hasExploded) {
-			Explode();
-			hasExploded = true;
-		}
-	}
+    void Explode()
+    {
+        GameObject impactEffectClone = Instantiate(impactEffect, transform.position, transform.rotation);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, blastRadius);
+        foreach (Collider2D nearbyObject in colliders)
+        {
+            Enemy enemy = nearbyObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+        Destroy(gameObject);
+        Destroy(impactEffectClone, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown = countdown - Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
 
 }
