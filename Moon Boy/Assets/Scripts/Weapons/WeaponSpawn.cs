61bd33fa6af67056434501bf72f9c172b0a346dd
Spencer Stream <sprewstream@gmail.com>
2019-12-31 11:40:45 -0600

    Adding a method which sets the range for the amount of ammo dropped by an enemy

--- Moon Boy/Assets/Scripts/Weapons/WeaponSpawn.cs
+++ Moon Boy/Assets/Scripts/Weapons/WeaponSpawn.cs
@@ -7,10 +7,23 @@ public class WeaponSpawn : MonoBehaviour
     public Weapon weapon;
     public int ammo;
 
+    private float minimumRange = 0f;
+    private float maximumRange = 50f;
+
+
+    private void SetRange(Weapon weapon)
+    {
+        if (weapon.name.Contains("Auto Cannon"))
+        {
+            minimumRange = 50f;
+            maximumRange = 100f;
+        }
+    }
 
     public void Initialize(Weapon weaponToSpawn) {
         weapon = weaponToSpawn;
-        int newAmmo = (int) Mathf.Floor(Random.Range(0f, 50f));
+        SetRange(weaponToSpawn);
+        int newAmmo = (int) Mathf.Floor(Random.Range(minimumRange, maximumRange));
         ammo = newAmmo;
     }
 
