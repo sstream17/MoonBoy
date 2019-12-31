2363a55feb0aace9b6fd53eb67274e54f6783962
Spencer Stream <sprewstream@gmail.com>
2019-12-31 11:11:18 -0600

    Fixed ShootArea to allow for aiming while shooting rapid fire

--- Moon Boy/Assets/Scripts/Weapons/ShootArea.cs
+++ Moon Boy/Assets/Scripts/Weapons/ShootArea.cs
@@ -1,21 +1,50 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ShootArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ShootArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IEndDragHandler
 {
     public PrefabWeapon PrefabWeapon;
     public bool buttonPressed;
     public Vector2 pointerPosition;
 
+    private Vector2 staticPointerPosition;
+    private Vector2 draggingPointerPosition;
+    private bool dragging = false;
+
+    private void SetPointerPosition()
+    {
+        if (dragging)
+        {
+            pointerPosition = draggingPointerPosition;
+        }
+        else
+        {
+            pointerPosition = staticPointerPosition;
+        }
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         buttonPressed = true;
-        pointerPosition = eventData.pressPosition;
+        staticPointerPosition = eventData.position;
+        SetPointerPosition();
         PrefabWeapon.SendShoot();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         buttonPressed = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragging = true;
+        draggingPointerPosition = eventData.position;
+        SetPointerPosition();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragging = false;
     }
 }
